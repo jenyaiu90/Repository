68ddf4b3a1c153b0e611d0093e41921c7eb162dc
Evgeny <iusko.jenya@yandex.ru>
2019-10-15 21:54:50 +0300

    1.1.3. Запуск на устройстве

--- app/src/main/java/MyProgram.java
+++ app/src/main/java/MyProgram.java
@@ -1,5 +1,7 @@
-public class MyProgram{
-	public static void main(String[] args){
+public class MyProgram
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello, world!");
 	}
 }
