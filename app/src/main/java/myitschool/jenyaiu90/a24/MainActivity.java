63a527207798f8a0a23f05136d3bed0380a0c63a
Evgeny <iusko.jenya@yandex.ru>
2019-11-21 20:42:31 +0300

    2.4.2. Создаём Android-проект

---
+++ app/src/main/java/myitschool/jenyaiu90/a24/MainActivity.java
@@ -0,0 +1,16 @@
+package myitschool.jenyaiu90.a24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
